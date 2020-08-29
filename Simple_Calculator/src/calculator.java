5a079a8d2cb24564aa2b5e84c3d218a78f852396
shantorohmat <59616417+shantorohmat@users.noreply.github.com>
2020-08-29 22:23:46 +0600

    hello
    Go away

---
+++ Simple_Calculator/src/calculator.java
@@ -0,0 +1,10 @@
+
+public class calculator {
+    public static void main(String[] args) {
+        Simple_calculator cal = new Simple_calculator();
+        cal.setVisible(true);
+        cal.setBounds(450,150,500,500);
+        cal.setResizable(false);
+    }
+    
+}
